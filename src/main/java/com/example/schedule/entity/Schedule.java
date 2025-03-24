e8a3ef2b0baf3d956a8c280c6fab509c6d4bc236
0122-0 <flowercats95@gmail.com>
2025-03-24 16:25:51 +0900

    Schedule 클래스 정의 및 생성자 구현

--- src/main/java/com/example/schedule/entity/Schedule.java
+++ src/main/java/com/example/schedule/entity/Schedule.java
@@ -1,2 +1,31 @@
 package com.example.schedule.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+public class Schedule {
+
+    private Long id;          // 임의의 추가 숫자
+    private String name;      // VARCHAR = String
+    private Date dating;      // Date = YYYY-MM-DD 의 형태
+    private String comment;   // VARCHAR = String
+    private Date createdAt;   // Date = YYYY-MM-DD 의 형태
+    private Date updatedAt;   // Date = YYYY-MM-DD 의 형태
+    private String password;  // VARCHAR = String
+
+//    public Schedule () {
+//        this.id = id;
+//        this.name = name;
+//        this.dating = dating;
+//        this.comment = comment;
+//        this.createdAt = createdAt;
+//        this.updatedAt = updatedAt;
+//        this.password = password;
+//    }
+
+
+}
