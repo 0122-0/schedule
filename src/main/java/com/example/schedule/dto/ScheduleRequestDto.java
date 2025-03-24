484c83361d4798913f1baf70d5b9eae066a18471
0122-0 <flowercats95@gmail.com>
2025-03-24 15:57:37 +0900

    최초 등록

--- src/main/java/com/example/schedule/dto/ScheduleRequestDto.java
+++ src/main/java/com/example/schedule/dto/ScheduleRequestDto.java
@@ -1,13 +1,3 @@
 package com.example.schedule.dto;
 
-import lombok.Getter;
 
-@Getter
-public class ScheduleRequestDto {
-
-    private String name;
-    private Long dating;
-    private String comment;
-    private char password;
-
-}
--- src/main/java/com/example/schedule/dto/ScheduleResponseDto.java
+++ src/main/java/com/example/schedule/dto/ScheduleResponseDto.java
@@ -1,23 +1,3 @@
 package com.example.schedule.dto;
 
-import com.example.schedule.entity.Schedule;
-import lombok.Getter;
 
-@Getter
-public class ScheduleResponseDto {
-
-    private String name;
-    private Long dating;
-    private String comment;
-    private Long creat_at;
-    private Long update_at;
-
-    public ScheduleDto(Schedule schedule){
-        this.name = schedule.getName();
-        this.dating = schedule.getDating();
-        this.comment = schedule.getComment();
-        this.creat_at = schedule.getCreat_at();
-        this.update_at = schedule.getUpdate_at();
-    }
-
-}
